57b4e3375a0632d1cc6ebc8d879f6e8e0a2c4c68
zfpx <zfpx@126.com>
2016-01-29 19:55:17 +0800

    8.完成了购物车界面

--- desktop/src/pages/cart/cart.controller.js
+++ desktop/src/pages/cart/cart.controller.js
@@ -1,7 +1,21 @@
 var CartController = ($scope, cartService) => {
     $scope.carts = cartService.query();
-    $scope.add = () => {
+    $scope.delete = (cart) => {
+        console.log(cart);
+        $scope.carts = $scope.carts.filter(function(curr){
+            return curr._id != cart._id;
+        });
+    }
+    $scope.total = () => {
+      var sum = 0;
+      $scope.carts.forEach(function(cart){
+          sum+=(cart.ware.price * cart.quantity);
+      });
+      return sum;
+    }
 
+    $scope.settle = () => {
+        alert('结算完成');
     }
 
 };
