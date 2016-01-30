d2012a403b76958fc8bc61499c80e464a0b68a30
zfpx <zfpx@126.com>
2016-01-31 01:21:03 +0800

    10.修改了bootstrap引入方式

--- admin/app.js
+++ admin/app.js
@@ -1,3 +1,5 @@
+require('expose?$!expose?jQuery!jquery');
+require("bootstrap-webpack");
 import angular from 'angular'
 import uiRouter from 'angular-ui-router'
 import ngResource from 'angular-resource'
--- admin/webpack.config.js
+++ admin/webpack.config.js
@@ -34,7 +34,11 @@ module.exports = {
                 test: /\.less$/,
                 exclude: /node_modules/,
                 loader: "style!css!less?strictMath&noIeCompat"
-            }
+            },
+            {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
+            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
         ]
     },
     plugins: [
